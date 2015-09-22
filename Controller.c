88fa6dff8e1218a83142c61396af437eb5c91af5
MikeVezina <Vezina95@Bell.net>
2015-09-22 14:54:14 -0400

    Child Process
    - The application now creates a child process

--- Controller.c
+++ Controller.c
@@ -3,7 +3,7 @@
  *
  *	The Controller contains two main components.
  *
- *	1. The Parent process will communicate with the cloud component (using FIFO -> the cloud = server & parent process = client)
+ *	1. The Parent process (CloudCommunicator) will communicate with the cloud component (using FIFO -> the cloud = server & parent process = client)
  *		- The parent process may catch a signal sent by the child process and read a message from the queue. It sends the info
  *				to the cloud process
  *		- The parent process may receive commands from the user (the cloud process).
@@ -12,7 +12,7 @@
  *				- put =
  *
  *
- *	2. The Child process will be used to send and receive device messages (from sensors or actuators) using a message queue
+ *	2. The Child process (DeviceCommunicator) will be used to send and receive device messages (from sensors or actuators) using a message queue
  *		- The first message of the device is a registration message. The child process registers this device (using PID, device name and threshold (if available)
  *		- Needs to echo any information it receives from any process
  *		- ... (See assignment hand out)
@@ -22,11 +22,47 @@
  *  Created on: Sep 22, 2015
  *      Author: MVezina
  */
+
 #include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
 
 int main(int argsv, char *args[]){
 
-	// Start off the assignment
+	// Create the child process through fork()
+
+
+	pid_t pid = fork();
+
+
+	// Use switch cases to determine if fork completed successfully, and to determine
+	// who the parent and child processes are
+
+	switch(pid)
+	{
+		// fork failed
+		case -1:
+
+			printf("Fork failed with error code %d. The Controller will quit.", errno);
+			return errno;
+
+		// Child Process
+		case 0:
+			printf("Yes, this is the child.");
+			break;
+
+		// Parent process where pid = PID of child
+		default:
+			printf("This is the parent. If you're looking for the child, its at PID: %d.", pid);
+
+			break;
+	}
+
+
+
+
+	return 0;
 
 
 }

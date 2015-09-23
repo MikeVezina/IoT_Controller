6b541c570fda75500c2b8c2f18e5d9a80d876f6a
MikeVezina <Vezina95@Bell.net>
2015-09-22 20:32:05 -0400

    Added DeviceInfo Struct
    - Includes name (szName), PID, and a threshold, and a char to represent
    whether or not the device contains a threshold

--- Structs.h
+++ Structs.h
@@ -9,5 +9,17 @@
 #define STRUCTS_H_
 
 
+// Create a struct for registered device information
+
+struct DeviceInfo
+{
+	pid_t pid = 0;
+	char* szDeviceName = (char *)0;
+	char hasThreshold = 0x0;
+	int threshold = 0;
+
+} typedef DeviceInfo;
+
+
 
 #endif /* STRUCTS_H_ */

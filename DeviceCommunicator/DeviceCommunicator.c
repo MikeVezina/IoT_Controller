31e2b49c8ee829fba4e72f4a40229079d0a894b3
MikeVezina <Vezina95@Bell.net>
2015-09-26 06:41:09 -0400

    Changed and Added Function Definitions
    - Changed isRegisteredDevice() to getRegisteredDevice()
    	- Function now returns a pointer to a registered device information
    structure or returns null if not found
    	- Added ProcessSensorMessage() function which processes sensor
    information sent to the DeviceCommunicator
    	- Added Print functions to print Device and Sensor information to
    stdout
    	- Added SendActCommand() function to send actuator commands
    	- Added Various error checks to functions

--- DeviceCommunicator/DeviceCommunicator.c
+++ DeviceCommunicator/DeviceCommunicator.c
@@ -38,18 +38,28 @@ int RegisterDevice(DEVICEINFO *deviceInfo)
 	pDevLink->nextDeviceLink = 0;
 
 	// If the head device is null, set it to the devicelink
-	if (!headDevice)
+	if (!headRegisteredDevice)
 	{
-		headDevice = pDevLink;
+		headRegisteredDevice = pDevLink;
 		return 0;
 	}
 
 	// Iterate to the last link, checking to see if the device PID already exists
-	PDEVICELINK pCurrentLink = headDevice;
+	PDEVICELINK pCurrentLink = headRegisteredDevice;
+
+	// Makes sure the device pid does not exist in the head device
+	if (pCurrentLink->devInfo.pid == pDevLink->devInfo.pid)
+	{
+		free(pDevLink);
+		return -1;
+	}
+
+	// Iterates until the last element is pointed to by pCurrentLink
 	while (pCurrentLink->nextDeviceLink)
 	{
-		// If the device PID already exists, dont register the device and return error -1
-		if (pCurrentLink->devInfo.pid == deviceInfo->pid)
+
+		// If the device PID already exists in an element in the list, dont register the device and return error -1
+		if (pCurrentLink->devInfo.pid == pDevLink->devInfo.pid)
 		{
 			free(pDevLink);
 			return -1;
@@ -65,23 +75,25 @@ int RegisterDevice(DEVICEINFO *deviceInfo)
 }
 
 // Returns 0 if the device pid is NOT a registered device
-// Returns 1 if the device pid IS a registered device
-int isRegisteredDevice(pid_t devicePid)
+// Returns the pointer to the DeviceInfo in a DeviceLink that is part of the registered devices linked list
+DEVICEINFO *getRegisteredDevice(pid_t devicePid)
 {
 	// If the head device is null, the device pid is not registered
-	if (!headDevice)
-		return 0;
+	if (!headRegisteredDevice)
+	{
+		return (void *) 0;
+	}
 
 	// Iterates through linked list to check if the
-	PDEVICELINK currentLink = headDevice;
+	PDEVICELINK currentLink = headRegisteredDevice;
 	while (currentLink)
 	{
 		if (currentLink->devInfo.pid == devicePid)
-			return 1;
+			return &currentLink->devInfo;
 
 		currentLink = currentLink->nextDeviceLink;
 	}
-	return 0;
+	return (void *) 0;
 
 }
 
@@ -91,7 +103,7 @@ void CheckForMessages()
 	// Do seperate checks for each of the different types of messages
 
 	// Check for Command Messages
-	COMMANDMESSAGE cmd;
+	PROCESSCOMMANDMESSAGE cmd;
 	if (!ReceiveMessage(&cmd, sizeof(cmd) - sizeof(cmd.msgHdr.msgType), MSG_CMD))
 	{
 
@@ -99,11 +111,11 @@ void CheckForMessages()
 		{
 			case CMD_REGACK: /* Device Registration Acknowledged */
 				break;
-			case CMD_QUIT: /* Device Registration Acknowledged */
-				fprintf(stdout, "The Device Communicator is now Exiting.\n");
+			case CMD_QUIT: /* Request To Quit Application */
+				fprintf(stdout, "PID %d: The Device Communicator is now Exiting. (Requested By PID: %d)\n\n", getpid(), cmd.msgHdr.sourcePid);
 				Quit();
 				break;
-			case CMD_FORCEQUIT:
+			case CMD_FORCEQUIT: /* Request To Force Quit */
 				ForceQuit();
 				break;
 		}
@@ -116,31 +128,172 @@ void CheckForMessages()
 	{
 		if (!RegisterDevice(&regMsg.devInfo))
 		{
-			fprintf(stdout, "Registered Device: %s\n", regMsg.devInfo.devName);
+			fprintf(stdout, "PID %d: A New Device Has Been Registered!\n", getpid());
+			PrintDeviceInfo(&regMsg.devInfo);
+			printf("\n");
 
-			COMMANDMESSAGE cmdMsg;
+			// Create a device registration acknowledgement command
+			PROCESSCOMMANDMESSAGE cmdMsg;
 			SetMessageHeader(&cmdMsg.msgHdr, regMsg.devInfo.pid, MSG_CMD);
 			cmdMsg.command[0] = CMD_REGACK;
 
-			// Now we want to send a device acknowledgement telling the device it is registered
+			// Sends the Registration acknowledgement to the device
+			if (SendMessage(&cmdMsg, sizeof(cmdMsg) - sizeof(cmdMsg.msgHdr.msgType)))
+			{
+				fprintf(stderr, "PID %d: Device ACK Failed. SendMessage error: %d", getpid(), errno);
+			}
+
 		}
 	}
-
+	// Receives and Processes Sensor Data message
 	SENSORDATAMESSAGE senMsg;
 	if (!ReceiveMessage(&senMsg, sizeof(senMsg) - sizeof(senMsg.msgHdr.msgType), MSG_SENINF))
 	{
-		//ProcessSensorMessage();
-		fprintf(stdout, "Sensor Data Received from: %s\n", regMsg.devInfo.devName);
+		ProcessSensorMessage(&senMsg);
 	}
 
+}
+
+// Processes the sensor message and any sensor information
+void ProcessSensorMessage(SENSORDATAMESSAGE *senMsg)
+{
+	// Ensure senMsg is not null
+	if (!senMsg)
+	{
+		fprintf(stderr, "ProcessSensorMessage(): *senMsg is null");
+		return;
+	}
+
+	// Get the device info from the received sensor message
+	DEVICEINFO *devInfo = getRegisteredDevice(senMsg->msgHdr.sourcePid);
+
+	// If devInfo is null, return without processing the message
+	if (!devInfo)
+	{
+		fprintf(stdout, "PID %d: The sensor message does not correspond to a registered Device PID (PID = %d)\n", getpid(), senMsg->msgHdr.sourcePid);
+		return;
+	}
+
+	// Ensure that only devices that are sensors can send sensor information messages
+	if (devInfo->devType != DEVTYPE_SENSOR)
+	{
+		fprintf(stdout, "PID %d: A Non-Sensor Device Has Sent a Sensor Information Message! (Only sensors can send sensor information messages)", getpid());
+		return;
+	}
+
+	// Print Sensor Info Received
+	fprintf(stdout, "PID %d: Sensor Data Received!\n", getpid());
+
+	// Print Device Information
+	PrintDeviceInfo(devInfo);
+
+	// Process The Sensor Information
+	PrintSensorInfo(&senMsg->sensorInfo);
+
+	fprintf(stdout, "\n");
+
+	// If the device has a threshold, then check if that threshold has been exceeded
+	if (devInfo->hasThreshold)
+	{
+		if (senMsg->sensorInfo.data[0] > devInfo->threshold)
+		{
+			printf("** Threshold Exceeded! **\n");
+			PrintDeviceInfo(devInfo);
+			printf("\n   Current Value (From Sensor): %d\n\n", senMsg->sensorInfo.data[0]);
+
+			switch (devInfo->thresholdAction)
+			{
+				case THAC_AC_OFF:
+				case THAC_AC_ON:
+					SendActCommand(ACTTYPE_AC_POWER_SWITCH, devInfo->thresholdAction);
+					break;
+
+				case THAC_BELL_RING:
+					SendActCommand(ACTTYPE_BELL, devInfo->thresholdAction);
+					break;
+
+				case THAC_FIRE_ALARM_RING:
+					SendActCommand(ACTTYPE_FIRE_ALARM, devInfo->thresholdAction);
+					break;
+
+				case THAC_LIGHT_OFF:
+				case THAC_LIGHT_ON:
+					SendActCommand(ACTTYPE_LIGHT_SWITCH, devInfo->thresholdAction);
+					break;
+
+					// Default action is to do nothing
+				case THAC_NONE:
+				default:
+					break;
+
+			}
+
+		}
+	}
+
+	fprintf(stdout, "\n");
+}
+
+
+
+// Sends a Command To An Actuator
+void SendActCommand(ActuatorType actType, ThresholdAction threshAct)
+{
+	printf("Actuator Type: %d, Action: %d\n", actType, threshAct);
+
+	// It is assumed that if we have more than one of the same type of actuator,
+	// That the Threshold action will be applied to all of the actuators. For example:
+	// If smoke is detected, turn on ALL Sprinklers
+
+	ACTUATORCOMMANDMESSAGE actCmd;
+	SetMessageHeader(&actCmd.msgHdr, 0, MSG_ACTCMD);
+
+	actCmd.commandSequence = currentCommandSequence++;
+	actCmd.threshAction = threshAct;
+
+	if(!SendMessage((void *) &actCmd, sizeof(actCmd) - sizeof(actCmd.msgHdr)))
+	{
+		printf("Command Sent!");
+	}
+
+
+}
+
+
+
+// Prints the device info to stdout
+void PrintDeviceInfo(DEVICEINFO *devInfo)
+{
+	if (devInfo == 0)
+		return;
+
+	// Print out the different fields of device information
+	fprintf(stdout, "- Device Information:\n   Device PID:\t%d\n   Device Name:\t%s\n   Device Type:\t%d\n   Threshold? \t%c\n", devInfo->pid, devInfo->devName, devInfo->devType, (devInfo->hasThreshold) ? 'Y' : 'N');
+
+	// Only print the device threshold if the device has a threshold
+	if (devInfo->hasThreshold)
+		fprintf(stdout, "   Threshold:\t%d", devInfo->threshold);
+
+	fprintf(stdout, "\n");
+}
+
+// Prints the Sensor Data to stdout
+void PrintSensorInfo(SENSORINFO *senInfo)
+{
+	if (senInfo == 0)
+		return;
+
+	fprintf(stdout, "\n- Sensor Info:\n");
+	for (int i = 0; i < (sizeof(senInfo->data) / sizeof(senInfo->data[0])); i++)
+	{
+		fprintf(stdout, "   [%d]: %d\n", i, senInfo->data[i]);
+	}
 }
 
 // Function to release all resources and exit
 void Quit()
 {
-	// TODO: Before exiting, ensure that all other messages in the message queue are processed
-
-	// After ensuring all of the messages are processed, ForceQuit is called
+// After ensuring all of the messages are processed, ForceQuit is called
 	ForceQuit();
 }
 
@@ -148,17 +301,16 @@ void Quit()
 void ForceQuit()
 {
 
-	// Free all memory allocated for device linked list
-	if (headDevice)
+// Free all memory allocated for device linked list
+	if (headRegisteredDevice)
 	{
-		PDEVICELINK pCurrentLink = headDevice;
-		// Iterate until the last link
+		PDEVICELINK pCurrentLink = headRegisteredDevice;
+
+		// Iterate through linked list and free memory allocated
 		PDEVICELINK pNextLink = pCurrentLink->nextDeviceLink;
 
 		while (pCurrentLink)
 		{
-
-			printf("Freeing Device.. %s\n", pCurrentLink->devInfo.devName);
 			free(pCurrentLink);
 
 			if (!pNextLink)

6c55856b2e807c1b37ebf3446d34e6d03204bd12
MikeVezina <Vezina95@Bell.net>
2015-09-26 03:21:42 -0400

    Created Function For Testing The Message Queue
    - Registers, Checks for ack, sends sensor data for testing purposes

--- Controller.c
+++ Controller.c
@@ -15,7 +15,7 @@
  *	2. The Child process (DeviceCommunicator) will be used to send and receive device messages (from sensors or actuators) using a message queue
  *		- The first message of the device is a registration message. The child process registers this device (using PID, device name and threshold (if available)
  *		- Needs to echo any information it receives from any process
- *		- ... (See assignment hand out)
+ *
  *
  *
  *
@@ -33,86 +33,100 @@
 #include <sys/wait.h>
 #include <string.h>
 
+
 #include "CloudCommunicator/CloudCommunicator.h"
 #include "DeviceCommunicator/DeviceCommunicator.h"
 #include "Messages/Messages.h"
-
+#include "Devices/Devices.h"
 
 
 char controllerName[25];
 
 void TestMessageSystem()
 {
+	pid_t devComPid;
+
 	DEVICEREGISTRATIONMESSAGE devMsg;
-	strcpy(devMsg.devInfo.devName, "asd");
+	SetMessageHeader((void *)&devMsg,0,MSG_DEVREG);
+
+	// Set Device Name
+	strcpy(devMsg.devInfo.devName, "TEMPSENSOR1");
+
+
+	// Set Device PID and type
 	devMsg.devInfo.pid = getpid();
-	devMsg.msgHdr.msgType = MSG_DEVREG;
+	devMsg.devInfo.devType = DEVTYPE_SENSOR;
+	devMsg.devInfo.sensType = SENSTYPE_TEMPERATURE;
+	devMsg.devInfo.hasThreshold = 1;
+	devMsg.devInfo.threshold = 25;
+	devMsg.devInfo.thresholdAction = THAC_AC_ON;
 
 	// Send a message to the message queue
 	if (!SendMessage((void *) &devMsg, sizeof(devMsg) - sizeof(devMsg.msgHdr.msgType)))
 	{
-		printf("PID %d: Device Registration Message Sent\n", getpid());
+		printf("PID %d: Device Registration Message Sent.\n-- Waiting for Registration Acknowledgement --\n\n", getpid());
 	}
 
-	strcpy(devMsg.devInfo.devName, "asd2");
 
-	// Send a message to the message queue
-	if (!SendMessage((void *) &devMsg, sizeof(devMsg) - sizeof(devMsg.msgHdr.msgType)))
-	{
-		printf("PID %d: Device Registration Message Sent\n", getpid());
-	}
+	PROCESSCOMMANDMESSAGE cmd;
 
-	strcpy(devMsg.devInfo.devName, "asd3");
+	// Wait for device registration ack from DeviceCommunicator
+	while(ReceiveMessage(&cmd,sizeof(cmd)-sizeof(cmd.msgHdr.msgType),MSG_CMD) || cmd.command[0] != CMD_REGACK);
 
-	// Send a message to the message queue
-	if (!SendMessage((void *) &devMsg, sizeof(devMsg) - sizeof(devMsg.msgHdr.msgType)))
-	{
-		printf("PID %d: Device Registration Message Sent\n", getpid());
-	}
+	// Ack has been recieved. The device is now registered!
+	printf("PID %d: -- Ack Received! --\n",getpid());
+
+	// Set the devComPid to source pid
+	devComPid = cmd.msgHdr.sourcePid;
+
+
 
 	SENSORDATAMESSAGE sdm;
-	sdm.msgHdr.msgType = MSG_SENINF;
+	SetMessageHeader((void *)&sdm,devComPid,MSG_SENINF);
+	sdm.sensorInfo.data[0] = 26;
+
 	// Send a message to the message queue
 	if (!SendMessage((void *) &sdm, sizeof(sdm) - sizeof(sdm.msgHdr.msgType)))
 	{
-		printf("PID %d: Device Sensor Message Sent\n", getpid());
+		printf("PID %d: Device Sensor Message Sent\n\n", getpid());
 	}
 
-	sleep(2);
+	sleep(1);
 
-	COMMANDMESSAGE cmdMsg;
-	cmdMsg.msgHdr.msgType = MSG_CMD;
-	cmdMsg.command[0] = 0xE;
-	cmdMsg.msgHdr.sourcePid = getpid();
+	PROCESSCOMMANDMESSAGE cmdMsg;
+	SetMessageHeader(&cmdMsg.msgHdr, devComPid, MSG_CMD);
+	cmdMsg.command[0] = CMD_QUIT;
+
 
 	// Send a message to the message queue
-	if (msgsnd(msqid, (void *) &cmdMsg, sizeof(cmdMsg) - sizeof(cmdMsg.msgHdr.msgType), 0) == -1)
+	if (!SendMessage((void *) &cmdMsg, sizeof(cmdMsg) - sizeof(cmdMsg.msgHdr.msgType)))
 	{
-		fprintf(stderr, "msgsnd failed, err: %d\n", errno);
-	}
-	else
-	{
-		printf("PID %d: Command Message Sent\n", getpid());
+		printf("PID %d: Quit Command Sent To %d\n", getpid(), cmdMsg.msgHdr.destinationPid);
+
 	}
 }
 
 int main(int argsv, char *args[])
 {
+	// Checks to make sure number of arguments is 2 for controller name
 	if (argsv != 2)
 	{
-		printf("Controller usage: controller [ControllerName (<= 24 chars)]");
+		printf("Usage: IoT_Controller [ControllerName]\n\t[ControllerName]: The name of the controller. Maximum of 24 Characters\n");
 		exit(1);
 	}
 
+	// Ensures Controller Name is 24 or less characters
 	if (strlen(args[1]) > 24)
 	{
-
 		fprintf(stdout, "The controller name must be a maximum of 24 characters\n");
 		exit(1);
 	}
+
+	// Copy Controller Name argument into controller name variable
 	strcpy(controllerName, args[1]);
 
-	printf("Controller %s: has started!\n", controllerName);
+	// Controller has started message
+	printf("========================================\nController '%s': has started!\n", controllerName);
 
 	// Initialize the message queue
 	msqid = msgget((key_t) MSGKEY, 0666 | IPC_CREAT);
@@ -150,7 +164,7 @@ int main(int argsv, char *args[])
 				// Therefore 500*10^3 = 500 milliseconds = 0.5 seconds
 				usleep(500 * (10 ^ 3));
 
-				// We can exit out of the child process by sending it an exit command
+				// NOTE: We can exit out of the child process by sending it an exit command
 			}
 
 			break;
@@ -159,10 +173,12 @@ int main(int argsv, char *args[])
 		default:
 		{
 			// Print Child and Parent PID
-			printf("Child PID: %d\nParent PID: %d\n", childpid, getpid());
+			printf("Child PID: %d\nParent PID: %d\n========================================\n\n", childpid, getpid());
 
+			// Tests the message system by sending test messages
 			TestMessageSystem();
 
+			// Wait for child process to exit
 			wait((void*) 0);
 
 			break;

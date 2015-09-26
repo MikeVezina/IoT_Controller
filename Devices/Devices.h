26e3af241d51e258f2df296b3cc139c7e6a159ed
MikeVezina <Vezina95@Bell.net>
2015-09-26 05:56:57 -0400

    Added Various Enums for Different Types of Sensors and Actuators

--- Devices/Devices.h
+++ Devices/Devices.h
@@ -8,8 +8,51 @@
 #ifndef DEVICES_H_
 #define DEVICES_H_
 
-#define DEVTYPE_SENSOR 1
-#define DEVTYPE_ACTUATOR 2
+/* --- Device Enums --- */
+
+// (DEV)ice (TYPE). The Device Type
+enum DeviceType{
+	DEVTYPE_SENSOR = 1,
+	DEVTYPE_ACTUATOR = 2
+} typedef DeviceType;
+
+// (TH)reshold (AC)tions. Actions that are executed when a threshold is exceeded
+enum ThresholdAction
+{
+	THAC_NONE = 0x0,
+	THAC_AC_OFF = 0x01,
+	THAC_AC_ON = 0x02,
+	THAC_BELL_RING = 0x04,
+	THAC_FIRE_ALARM_RING = 0x08,
+	THAC_LIGHT_OFF = 0x10,
+	THAC_LIGHT_ON = 0x20
+
+} typedef ThresholdAction;
+
+// (SENS)or (TYPE). The type of sensor
+enum SensorType{
+	SENSTYPE_TEMPERATURE = 11,
+	SENSTYPE_SMOKE_DETECTOR = 12,
+	SENSTYPE_LIGHT = 13
+}typedef SensorType;
+
+// (ACT)uator (TYPE). The type of actuator
+enum ActuatorType{
+	ACTTYPE_AC_POWER_SWITCH = 21,
+	ACTTYPE_BELL = 22,
+	ACTTYPE_FIRE_ALARM = 23,
+	ACTTYPE_WATER_SPRINKLER = 24,
+	ACTTYPE_LIGHT_SWITCH = 25
+
+}typedef ActuatorType;
+
+/* --- End Device Enums --- */
+
+
+
+
+
+/* --- Device Structures --- */
 
 // Structure for Device Info
 struct DEVICEINFO
@@ -21,13 +64,23 @@ struct DEVICEINFO
 	char devName[25];
 
 	// Device type
-	char devType;
+	DeviceType devType;
+
+	// If devType == DEVTYPE_SENSOR use SensorType enum
+	SensorType sensType;
+
+	// else, if devType == DEVTYPE_ACTUATOR use ActuatorType enum
+	ActuatorType actType;
 
 	// This value is either true ( != 0 ) or False ( = 0 ) to show that the device has a specified threshold
 	char hasThreshold;
 
 	// The threshold value that is looked at if and only if the 'hasThreshold' variable is != 0
 	int threshold;
+
+	// Threshold Action (Only used for sensors)
+	ThresholdAction thresholdAction;
+
 
 } typedef DEVICEINFO;
 
@@ -46,6 +99,10 @@ struct DEVICELINK
 
 } typedef DEVICELINK, *PDEVICELINK;
 
+/* --- End Device Structures --- */
+
+
+/* Device Function Prototypes */
 int SetDeviceInfo();
 
 #endif /* DEVICES_H_ */
